0dc100057926be63abf24d30cde53c8485cf1af1
Umedzhon Abdumuminov <ovegug@gmail.com>
2019-02-09 17:55:10 +0300

    Prepared boilerplate to start development (#2)

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: "./src/index.tsx"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", "json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.ts|tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src/index.html') }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
